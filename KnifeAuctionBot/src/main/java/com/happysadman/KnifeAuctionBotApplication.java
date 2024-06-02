444dcc9734ccb576ddacf0650145485490ebc1ea
HappySadAlex <85686694+HappySadAlex@users.noreply.github.com>
2024-06-02 19:57:26 +0300

    Initial commit

---
+++ KnifeAuctionBot/src/main/java/com/happysadman/KnifeAuctionBotApplication.java
@@ -0,0 +1,13 @@
+package com.happysadman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnifeAuctionBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnifeAuctionBotApplication.class, args);
+	}
+
+}
---
+++ KnifeAuctionBot/src/test/java/com/happysadman/KnifeAuctionBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happysadman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnifeAuctionBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
